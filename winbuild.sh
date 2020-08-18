306d9a45636696f65f532361c04126d00e738730
lemon-sherbet <tfefqwe@protonmail.com>
2020-08-18 21:20:27 +0200

    winbuild

--- winbuild.sh
+++ winbuild.sh
@@ -12,13 +12,13 @@ C () {
 }
 C mkdir -p win-build
 C cd win-build
-ZIGFLAGS="-target i386-windows-gnu -I $WSYSROOT/include/ -I $WSYSROOT/include/SDL/ -L $WSYSROOT/lib/ -Wno-ignored-attributes"
+ZIGFLAGS="-O2 -g -target i386-windows-gnu -I $WSYSROOT/include/ -I $WSYSROOT/include/SDL/ -L $WSYSROOT/lib/ -Wno-ignored-attributes"
 #CC="zig cc $ZIGFLAGS" CXX=zig\ c++ C make OUT=ccleste.exe -C ..
 C zig cc $ZIGFLAGS -c ../celeste.c -o celeste.o
 C zig cc $ZIGFLAGS -lSDLmain -lSDL  -lSDL_mixer celeste.o ../sdl12main.c -o ccleste.exe
 C zig c++ $ZIGFLAGS -DCELESTE_P8_FIXEDP -c -xc++ ../celeste.c -o celeste-fixedp.o
 C zig cc $ZIGFLAGS -lSDLmain -lSDL  -lSDL_mixer celeste-fixedp.o ../sdl12main.c -o ccleste-fixedp.exe
-C rm -r zig-cache *.o stdout.txt
+C rm -rf zig-cache *.o stdout.txt
 C cp $WSYSROOT/bin/libgcc_s_*.dll .
 C cp $WSYSROOT/bin/libogg*.dll .
 C cp $WSYSROOT/bin/libwinpthread*.dll .
